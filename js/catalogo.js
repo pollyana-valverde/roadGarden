127e2a525a8da9a67dfe53141fc00489622575dc
Pollyana Valverde <valverdepolly@gmail.com>
2025-02-20 21:59:07 -0300

    catalogo em andamento

---
+++ js/catalogo.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const tagMediumHome = document.getElementById("tagMediumHome");
+    const itensFiltrados = [
+        { geralCategoria: "Categoria 1", produtos: ["Produto 1A", "Produto 1B", "Produto 1C", "Produto 1C","Produto 1C","Produto 1C","Produto 1C","Produto 1C","Produto 1C",] },
+        { geralCategoria: "Categoria 2", produtos: ["Produto 2A", "Produto 2B", "Produto 2C"] },
+        { geralCategoria: "Categoria 3", produtos: ["Produto 3A", "Produto 3B", "Produto 3C"] }
+    ];
+
+    function renderCarrossel() {
+        tagMediumHome.innerHTML = "";
+        itensFiltrados.forEach((categoria, index) => {
+            const container = document.createElement("div");
+            container.innerHTML = `
+                <div class="catalogoCategory">
+                    <p>${categoria.geralCategoria}</p>
+                </div>
+                <div class="catalogoProdutos-container">
+                    <button class="scroll-button left" onclick="scrollTabs(this, 'left')">&#9664;</button>
+                    <div class="catalogoProdutos-content">
+                        ${categoria.produtos.map(produto => `
+                           <div class='contentProdutos'>
+                                <div class="imgProduto">
+                                    <img src="/imagens/jardim2.avif" 
+                                         alt="Banner ${Math.ceil((index + 1) / 3)}" 
+                                    />
+                                </div>
+                                <div class="nomeProduto">${produto}</div>
+                           </div>
+                            `).join('')}
+                    </div>
+                    <button class="scroll-button right" onclick="scrollTabs(this, 'right')">&#9654;</button>
+                </div>
+            `;
+            tagMediumHome.appendChild(container);
+        });
+    }
+
+    window.scrollTabs = function (button, direction) {
+        const container = button.parentElement.querySelector(".catalogoProdutos-content");
+        const scrollAmount = 700;
+        container.scrollBy({
+            left: direction === 'left' ? -scrollAmount : scrollAmount,
+            behavior: 'smooth',
+        });
+    };
+
+    renderCarrossel();
+});
